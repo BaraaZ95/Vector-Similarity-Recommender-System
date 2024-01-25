a534a6051a514d1a0a6a7516e97551fc1040fbb5
baraaz95 <archangle.650@gmail.com>
2024-01-25 15:20:53 +0400

    Initial commit

---
+++ recommender_api/services/__init__.py
@@ -0,0 +1,1 @@
+"""Services for recommender_api."""
---
+++ recommender_api/web/application.py
@@ -0,0 +1,36 @@
+from importlib import metadata
+
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+
+from recommender_api.logging import configure_logging
+from recommender_api.web.api.router import api_router
+from recommender_api.web.lifetime import register_shutdown_event, register_startup_event
+
+
+def get_app() -> FastAPI:
+    """
+    Get FastAPI application.
+
+    This is the main constructor of an application.
+
+    :return: application.
+    """
+    configure_logging()
+    app = FastAPI(
+        title="recommender_api",
+        version=metadata.version("recommender_api"),
+        docs_url="/api/docs",
+        redoc_url="/api/redoc",
+        openapi_url="/api/openapi.json",
+        default_response_class=UJSONResponse,
+    )
+
+    # Adds startup and shutdown events.
+    register_startup_event(app)
+    register_shutdown_event(app)
+
+    # Main router for the API.
+    app.include_router(router=api_router, prefix="/api")
+
+    return app
